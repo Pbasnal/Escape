a8669e02a1e7e557bc15c7918487338ea543d112
Pankaj Basnal <pbasnal@microsoft.com>
2019-09-12 22:04:37 +0530

    Started Enemy

---
+++ Assets/Scripts/Guards/GuardSimple.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardSimple : MonoBehaviour
+{
+    public Transform pathHolder;
+    public Transform torch;
+
+    public float speed = 1f;
+    public float waitTime = 0.3f;
+    public float turnSpeed = 1.0f;
+
+    private int currentWaypoint = 0;
+    void OnDrawGizmos()
+    {
+        var startPosition = pathHolder.GetChild(0).position;
+        var previousPosition = startPosition;
+        foreach (Transform waypoint in pathHolder)
+        {
+            Gizmos.DrawSphere(waypoint.position, 0.3f);
+            Gizmos.DrawLine(previousPosition, waypoint.position);
+            previousPosition = waypoint.position;
+        }
+        Gizmos.DrawLine(previousPosition, startPosition);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var waypoints = new Vector3[pathHolder.childCount];
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            waypoints[i] = pathHolder.GetChild(i).position;
+        }
+
+        torch.position = new Vector3(transform.position.x, transform.position.y, -0.01f);
+        torch.rotation = new Quaternion(transform.rotation.z, -90, -90, transform.rotation.w);
+
+        torch.parent = transform;
+
+        StartCoroutine(FollowPath(waypoints));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator TurnToFace(Vector3 lookTarget)
+    {
+        var dir = (lookTarget - transform.position).normalized;
+        float targetAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+
+        while (Mathf.DeltaAngle(transform.eulerAngles.z, targetAngle) > 0.05f)
+        {
+            float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.z, targetAngle, turnSpeed * Time.deltaTime);
+            transform.eulerAngles = Vector3.forward * angle;
+            yield return null;
+        }
+    }
+
+    IEnumerator FollowPath(Vector3[] waypoints)
+    {
+        int targetWaypointIndex = (currentWaypoint + 1) % waypoints.Length;
+        transform.Translate(waypoints[currentWaypoint]);
+        var targetWaypoint = waypoints[targetWaypointIndex];
+
+        var dir = (targetWaypoint - transform.position).normalized;
+        float targetAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+
+        float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.z, targetAngle, 100);
+        transform.eulerAngles = Vector3.forward * angle;
+
+        while (true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime);
+            if (transform.position == targetWaypoint)
+            {
+                targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
+                targetWaypoint = waypoints[targetWaypointIndex];
+                yield return new WaitForSeconds(waitTime);
+                yield return StartCoroutine(TurnToFace(targetWaypoint));
+            }
+            yield return null;
+        }
+    }
+}
